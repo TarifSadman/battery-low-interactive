82f1326ec5cc7d4b360e8650ea1c1e57a193a7dd
Tarif Sadman <84661593+TarifSadman@users.noreply.github.com>
2023-08-21 06:28:12 +0000

    fixed

--- src/PDF.js
+++ src/PDF.js
@@ -1,25 +1,45 @@
-// ResultPDF.js
 import React from 'react';
 import { Page, Document, Text, View, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
-    backgroundColor: '#E4E4E4',
+    backgroundColor: '#ffffff',
+    padding: 20,
   },
   section: {
-    marginBottom: 10,
+    marginBottom: 20,
+    borderBottom: '1px solid #cccccc',
+    paddingBottom: 15,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     marginBottom: 10,
+    fontWeight: 'bold',
+    color: '#333333',
   },
   label: {
     fontWeight: 'bold',
-    marginRight: 5,
+    marginBottom: 5,
+    color: '#666666',
   },
   value: {
-    marginLeft: 5,
+    color: '#333333',
+  },
+  gridContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginLeft: -5,
+    marginRight: -5,
+  },
+  gridItem: {
+    width: '48%',
+    marginBottom: 15,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
   },
 });
 
@@ -27,15 +47,66 @@ const ResultPDF = ({ formData }) => (
   <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
+        <Text style={styles.title}>Project Information</Text>
+        <View>
+          <Text>
+            <Text style={styles.label}>Project Name:</Text>
+            <Text style={styles.value}>{formData.projectName}</Text>
+          </Text>
+          <Text>
+            <Text style={styles.label}>Project Description:</Text>
+            <Text style={styles.value}>{formData.projectDescription}</Text>
+          </Text>
+          <Text>
+            <Text style={styles.label}>Client:</Text>
+            <Text style={styles.value}>{formData.client}</Text>
+          </Text>
+          <Text>
+            <Text style={styles.label}>Contractor:</Text>
+            <Text style={styles.value}>{formData.contractor}</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.section}>
         <Text style={styles.title}>Maximum and Minimum Values</Text>
-        {Object.keys(formData).map(key => (
-          <View key={key} style={styles.section}>
+        <View style={styles.gridContainer}>
+          <View style={styles.gridItem}>
             <Text>
-              <Text style={styles.label}>{key}: </Text>
-              <Text style={styles.value}>{formData[key]}</Text>
+              <Text style={styles.label}>Max X:</Text>
+              <Text style={styles.value}>{formData.max_X}</Text>
             </Text>
           </View>
-        ))}
+          <View style={styles.gridItem}>
+            <Text>
+              <Text style={styles.label}>Min X:</Text>
+              <Text style={styles.value}>{formData.min_X}</Text>
+            </Text>
+          </View>
+          <View style={styles.gridItem}>
+            <Text>
+              <Text style={styles.label}>Max Y:</Text>
+              <Text style={styles.value}>{formData.max_Y}</Text>
+            </Text>
+          </View>
+          <View style={styles.gridItem}>
+            <Text>
+              <Text style={styles.label}>Min Y:</Text>
+              <Text style={styles.value}>{formData.min_Y}</Text>
+            </Text>
+          </View>
+          <View style={styles.gridItem}>
+            <Text>
+              <Text style={styles.label}>Max Z:</Text>
+              <Text style={styles.value}>{formData.max_Z}</Text>
+            </Text>
+          </View>
+          <View style={styles.gridItem}>
+            <Text>
+              <Text style={styles.label}>Min Z:</Text>
+              <Text style={styles.value}>{formData.min_Z}</Text>
+            </Text>
+          </View>
+        </View>
       </View>
     </Page>
   </Document>
